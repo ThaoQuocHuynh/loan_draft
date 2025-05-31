c0223089fb4abb0c0cfc6c1d772ca6c88d6ee8f1
Thao Quoc Huynh <vn.thuynh@quantalytix.com>
2025-06-01 00:30:42 +0700

    Config basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 // react + libraries
-import { RouterProvider } from "react-router-dom"
-
+import { RouterProvider } from 'react-router-dom';
 // application components
-import { ContextProvider } from "./ContextProvider.tsx"
-import { router } from "./Routing.tsx"
+import { ContextProvider } from './ContextProvider.tsx';
+import { router } from './Routing.tsx';
 
 function App() {
   return (
     <ContextProvider>
       <RouterProvider router={router} />
     </ContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Routing.tsx
+++ src/Routing.tsx
@@ -11,70 +11,73 @@ import FormBuilderPage from './pages/FormBuilder';
 import FormRendererWrapper from './pages/FormBuilder/FormRenderer';
 import FormManager from './pages/FormBuilder/form-manager';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        // element: <DashboardEditor />,
-        element: <Navigate to="/form-manager" replace={true} />,
-      },
-      {
-        path: '/dashboard-editor/:id',
-        element: <DashboardEditor />,
-      },
-      {
-        path: '/dashboard-manager',
-        element: <DashboardManager />,
-      },
-      {
-        path: '/form-rerenderer',
-        element: <FormRendererWrapper />,
-      },
-      {
-        path: '/sidebar-manager',
-        element: <SidebarManager />,
-      },
-      {
-        path: '/interactive-editor',
-        element: <WorkflowEditor />,
-      },
-      {
-        path: '/interactive-workflows',
-        element: <WorkflowManager />,
-      },
-      {
-        path: '/permissions/roles',
-        element: <RolesPageEditor />,
-      },
-      {
-        path: '/permissions/claims',
-        element: <ClaimsPageEditor />,
-      },
-      {
-        path: '/permissions/policies',
-        element: <PoliciesPageEditor />,
-      },
-      {
-        path: '/users',
-        element: <UserManager />,
-      },
-      {
-        path: '/data-dictionary',
-        element: <DataDictionary />,
-      },
-      {
-        path: '/form-builder',
-        element: <FormBuilderPage />,
-      },
-      {
-        path: '/form-manager',
-        element: <FormManager />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          // element: <DashboardEditor />,
+          element: <Navigate to="/form-manager" replace={true} />,
+        },
+        {
+          path: '/dashboard-editor/:id',
+          element: <DashboardEditor />,
+        },
+        {
+          path: '/dashboard-manager',
+          element: <DashboardManager />,
+        },
+        {
+          path: '/form-rerenderer',
+          element: <FormRendererWrapper />,
+        },
+        {
+          path: '/sidebar-manager',
+          element: <SidebarManager />,
+        },
+        {
+          path: '/interactive-editor',
+          element: <WorkflowEditor />,
+        },
+        {
+          path: '/interactive-workflows',
+          element: <WorkflowManager />,
+        },
+        {
+          path: '/permissions/roles',
+          element: <RolesPageEditor />,
+        },
+        {
+          path: '/permissions/claims',
+          element: <ClaimsPageEditor />,
+        },
+        {
+          path: '/permissions/policies',
+          element: <PoliciesPageEditor />,
+        },
+        {
+          path: '/users',
+          element: <UserManager />,
+        },
+        {
+          path: '/data-dictionary',
+          element: <DataDictionary />,
+        },
+        {
+          path: '/form-builder',
+          element: <FormBuilderPage />,
+        },
+        {
+          path: '/form-manager',
+          element: <FormManager />,
+        },
+      ],
+    },
+  ],
+  { basename: '/loan_draft' }
+);
 
 export { router };
