0dd2082ac1160fecddd19ff2e7ebd26c3bba190f
Thao Quoc Huynh <vn.thuynh@quantalytix.com>
2025-05-31 23:25:54 +0700

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), visualizer({ open: false }) as PluginOption],
+  base: '/loan-os/',
   build: {
     rollupOptions: {
       output: {
